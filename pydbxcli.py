3d1e7723872ecf5cf110bad896aaf35577d4fce3
Luke Adams <ldadams@gmail.com>
2017-07-24 21:30:51 -0400

    added support for pushing ls to a queue and using that queue later for get

--- pydbxcli.py
+++ pydbxcli.py
@@ -5,6 +5,10 @@ import sys
 import argparse
 import signal
 import dropbox
+import json
+from queuelib import FifoDiskQueue
+from collections import namedtuple
+from datetime import datetime, date
 
 __version__ = 0.1
 
@@ -54,6 +58,10 @@ def main():
                                  "example: --excludePaths '/Team Folders/IgnoreThisFolder' "
                                  "'/Team Folders/IgnoreThisFolderToo'")
                            )
+    parser_ls.add_argument('--pushFilesToQueue',
+                         type=str,
+                         default=None,
+                         help='push files to the given queue name')
     parser_ls.set_defaults(func=ls)
 
     # create the parser for the "get" command
@@ -71,14 +79,29 @@ def main():
                            type=str,
                            default='.',
                            help='which path on the local machine to store downloads')
+    parser_ls.add_argument('--pullFilesFromQueue',
+                          type=str,
+                          default=None,
+                          help='pull files from the given queue name')
     parser_ls.add_argument('--excludePaths',
                            nargs='*',
                            default=[],
                            help=("a list of folder paths to ignore. "
                                  "example: --excludePaths '/Team Folders/IgnoreThisFolder' "
                                  "'/Team Folders/IgnoreThisFolderToo'")
-                           )
+                                 )
     parser_ls.set_defaults(func=get)
+
+    #add parser for peeking at queues
+    parser_ls = subparser.add_parser(name='peek',
+                                  help='peek at a queue')
+    parser_ls.add_argument('--queueName',
+                        help='Name of queue to peek')
+    parser_ls.add_argument('--flush',
+                        action='store_true',
+                        default=False,
+                        help='flush queue after peek')
+    parser_ls.set_defaults(func=peek)
 
     # parse the args and call the selected command function default to help and version
     args = parser.parse_args()
@@ -102,6 +125,17 @@ def status(args):
         print('{:>20}: {}\n'.format(attr, getattr(account, attr), ''))
     sys.exit(0)
 
+def peek(args):
+    if not args.queueName:
+        return;
+
+    queue = get_queue(args.queueName)
+    files = get_files_in_queue(queue)
+    for file in files.entries:
+        print(json.loads(file)['path_display'])
+
+    if args.flush:
+        queue.close()
 
 def ls(args):
     dbx = connect_to_dropbox(args)
@@ -109,8 +143,9 @@ def ls(args):
     if args.path == '/':
         args.path = ''
 
+    files = dbx.files_list_folder(path=args.path, recursive=args.r)
     while True:
-        files = dbx.files_list_folder(path=args.path, recursive=args.r)
+        queue = get_queue(args.pushFilesToQueue) if args.pushFilesToQueue else None
         for entry in files.entries:
             #skip any paths specified in --excludePaths
             if any(map(getattr(entry, 'path_display').startswith, args.excludePaths)):
@@ -120,63 +155,123 @@ def ls(args):
             print('{:>8}  {:>20}  {}'.format(sizeof_fmt(getattr(entry, 'size', 0)),
                                              str(getattr(entry, 'client_modified', '-')),
                                              getattr(entry, 'path_display', '-')))
+
+            # if we have a queue given create if needed and push the files to the queue
+            if queue != None and getattr(entry, 'size', None) != None:
+                file = FakeFile()
+                file.path_display = getattr(entry, 'path_display')
+                file.size = getattr(entry, 'size')
+                file.server_modified = json_serial(getattr(entry, 'server_modified'))
+                file.client_modified = json_serial(getattr(entry, 'client_modified'))
+                queue.push(str.encode(json.dumps(file.__dict__)))
+
         if files.has_more:
             files = dbx.files_list_folder_continue(files.cursor)
         else:
             break
 
+        #write batch
+        if queue:
+            queue.close()
 
-def get(args):
-    dbx = connect_to_dropbox(args)
+    #write queue to disk
+    if queue:
+        queue.close()
+
+def copy_dropbox_file(args, entry, dbx):
+    # skip empty files and directories
+    if not getattr(entry, 'size', None):
+        print('empty file or directory skipping ' + getattr(entry, 'path_display'))
+        return
+
+    # skip any paths specified in --excludePaths
+    if any(map(getattr(entry, 'path_display').startswith, args.excludePaths)):
+        print('Excluding {}'.format(getattr(entry, 'path_display')))
+        return
 
     if args.src_path == '/':
         args.src_path = ''
 
-    files = dbx.files_list_folder(path=args.src_path, recursive=args.r)
+    local_dir = os.path.dirname(args.dest_path + entry.path_display)
+    try:
+        if not os.path.exists(local_dir):
+            print('Creating directory {}'.format(local_dir))
+            os.makedirs(local_dir)
+    except Exception as err:
+        print(err)
+        sys.exit(1)
+
+    src = entry.path_display
+    dest = args.dest_path + entry.path_display
+
+    # we have a file so create local directory if necessary then download
+    print('Downloading {} to {}' .format(src, dest))
+
+    try:
+        dbx.files_download_to_file(path=src, download_path=dest)
+    except Exception as err:
+        print(err)
+        sys.exit(1)
+    # set atime/mtime for file
+    try:
+        modified = entry.server_modified
+        cModified = entry.client_modified
+
+        if type(entry.server_modified) is str:
+            modified = datetime.strptime(entry.server_modified, '%Y-%m-%dT%H:%M:%S')
+        if type(entry.client_modified) is str:
+            cModified = datetime.strptime(entry.client_modified, '%Y-%m-%dT%H:%M:%S')
+
+        os.utime(path=dest, times=(modified.timestamp(), cModified.timestamp()))
+    except Exception as err:
+        print(err)
+        sys.exit(1)
+
+
+def get(args):
+    dbx = connect_to_dropbox(args)
+    if args.pullFilesFromQueue:
+        while True:
+            queue = get_queue(args.pullFilesFromQueue)
+            entry = queue.pop()
+
+            if not entry:
+                break
+
+            # Parse JSON into an object with attributes corresponding to dict keys.
+            #https://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object
+            parsed = json.loads(entry, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+            copy_dropbox_file(args, parsed, dbx)
+            queue.close()
+    else:
+        dbx = connect_to_dropbox(args)
+        while True:
+            files = files = dbx.files_list_folder(path=args.src_path, recursive=args.r)
+
+            for entry in files.entries:
+                copy_dropbox_file(args, entry, dbx)
+
+            if files.has_more:
+                files = dbx.files_list_folder_continue(files.cursor)
+            else:
+                break
+
+def get_files_in_queue(queue):
+    result = FakeFileList()
+
+    if not queue:
+        print('queue not found')
+        return result
+
     while True:
-        for entry in files.entries:
-            # skip any paths specified in --excludePaths
-            if any(map(getattr(entry, 'path_display').startswith, args.excludePaths)):
-                print('Excluding {}'.format(getattr(entry, 'path_display')))
-                continue
+        file = queue.pop()
+        if not file:
+            return result;
 
-            size = getattr(entry, 'size', None)
-            # skip empty files and directories
-            if not size:
-                continue
+        result.entries.append(file.decode())
 
-            local_dir = os.path.dirname(args.dest_path + entry.path_display)
-            try:
-                if not os.path.exists(local_dir):
-                    print('Creating directory {}'.format(local_dir))
-                    os.makedirs(local_dir)
-            except Exception as err:
-                print(err)
-                sys.exit(1)
-
-            src = entry.path_display
-            dest = args.dest_path + entry.path_display
-
-            # we have a file so create local directory if necessary then download
-            print('Downloading {} to {}' .format(src, dest))
-
-            try:
-                dbx.files_download_to_file(path=src, download_path=dest)
-            except Exception as err:
-                print(err)
-                sys.exit(1)
-            # set atime/mtime for file
-            try:
-                os.utime(path=dest, times=(entry.server_modified.timestamp(), entry.client_modified.timestamp()))
-            except Exception as err:
-                print(err)
-                sys.exit(1)
-
-        if files.has_more:
-            files = dbx.files_list_folder_continue(files.cursor)
-        else:
-            break
-
+def get_queue(name):
+    return FifoDiskQueue(name)
 
 def connect_to_dropbox(args):
     # connect to dropbox
@@ -202,6 +297,23 @@ def sizeof_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    """https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable"""
+    if isinstance(obj, (datetime, date)):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+class FakeFileList(object):
+       has_more = False
+       entries = list()
+
+class FakeFile(object):
+      path_display = None
+      size = 0
+      server_modified = None
+      client_modified = None
 
 if __name__ == '__main__':
     main()
